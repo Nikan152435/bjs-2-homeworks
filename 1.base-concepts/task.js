7e30d7d6078a6cd8679bb6862f4e5048c4de3db7
NikaN <NikaN7602@gmail.com>
2023-07-27 22:23:33 +0200

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,14 @@
 "use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = Math.pow(b,2)-4*a*c;
+  let d = Math.pow(b, 2) - 4 * a * c;
  
   if (d > 0) {
-  let rootFirst = (-b + Math.sqrt(d) )/(2*a);
-  let rootSecond = (-b - Math.sqrt(d) )/(2*a);
+  let rootFirst = (-b + Math.sqrt(d)) / (2 * a);
+  let rootSecond = (-b - Math.sqrt(d)) / (2 * a);
   arr.push(rootFirst, rootSecond);
 
-} else if(d === 0) {
+} else if (d === 0) {
   let root = -b / (2 * a);
   arr.push(root);
 } 
