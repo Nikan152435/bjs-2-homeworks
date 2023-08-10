8e6763ad983b5dcf414d95f0fe38186d8e6f0098
NikaN <NikaN7602@gmail.com>
2023-08-10 22:23:38 +0200

    task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,4 +1,4 @@
-//Задача № 1Debounce декоратор с моментальным вызовом и подсчётом 
+//Задача № 1 Debounce декоратор с моментальным вызовом и подсчётом 
 //количества вызовов
  
 function debounceDecoratorNew(func, delay) {
@@ -26,3 +26,44 @@ module.exports = {
   }
 
   
+ //Задача 2 
+
+ const md5 = require('./js-md5.js');
+
+function cachingDecoratorNew(func) {
+  let cache = [];
+  const maxCacheValuesCount = 5;
+
+  return (...args) => {
+    const hash = args.join(",");
+    let searchingObjectInCache = cache.find((item) => {
+      if (hash in item) {
+        return item;
+
+      }
+
+    });
+
+    if (searchingObjectInCache) {
+      return "Из кеша: " + searchingObjectInCache[hash];
+
+    }
+
+    const result = func(...args);
+    let newObjest = {};
+    newObjest[hash] = result;
+    cache.push(newObjest);
+
+    if (cache.length > maxCacheValuesCount) {
+      cache.shift();
+
+    }
+
+    return "Вычисляем: " + result;
+    
+  }
+}
+
+module.exports = {
+  cachingDecoratorNew
+}
