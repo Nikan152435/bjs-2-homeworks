412c915e8941ac1caece9de6128cd30d35d2d1b2
NikaN <NikaN7602@gmail.com>
2023-08-10 02:26:47 +0200

    task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -5,5 +5,22 @@ function cachingDecoratorNew(func) {
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+
+    function wrapper(...args) {
+    wrapper.allCount++;
+    if (timeoutId === null) {
+        wrapper.count++;
+        func(...args)
+    }
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+    func(...args);
+    wrapper.count++;
+    }, delay);
+}
+
+return wrapper;
 }
